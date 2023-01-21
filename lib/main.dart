10da1450d45a8d94974fd323b7c2368492df5b91
julysNICK <julystube@gmail.com>
2023-01-21 14:31:01 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:cars_sale/screen/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
---
+++ lib/screen/Home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedItemBottom = 0;
+
+  onTapItem(int index) {
+    setState(() {
+      _selectedItemBottom = index;
+    });
+  }
+
+  final _screens = [
+    const Center(child: Text('Home')),
+    const Center(child: Text('Search')),
+    const Center(child: Text('Profile')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedItemBottom,
+          onTap: onTapItem,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
